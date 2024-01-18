a3dffd3a775ad2e32851afd8992eb0aa83165bdb
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-01-18 15:52:39 +0000

    update

--- .history/resources/views/components/application-logo.blade_20240118153723.php
+++
@@ -1,5 +0,0 @@
-@if(file_exists(base_path("/assets/liinkmee/images/avatar.png" )))
-    <img class="mb-5" src="{{ asset('/assets/liinkmee/images/avatar.png') }}"  style="width: 150px;">
-@else
-    <img class="mb-5" src="{{ asset('/assets/liinkmee/images/avatar@2x.png') }}">
-@endif
